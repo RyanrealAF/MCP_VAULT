2bd1ba9f7bc6f47acf6ed27c509aa19a5e2d0345
purarecoveryryan <purarecoveryryan@gmail.com>
2026-04-23 17:38:28 +0000

    ff

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -18,7 +18,12 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "dbaeumer.vscode-eslint",
+      "esbenp.prettier-vscode",
+      "vscode-icons-team.vscode-icons",
+      "eamodio.gitlens",
+      "github.copilot",
+      "bradlc.vscode-tailwindcss"
     ];
 
     # Enable previews
